927a104abf52ad3f9df2644492214b6a8a45a67c
Deepali Sharma <35188717+deepali99@users.noreply.github.com>
2025-06-10 19:13:20 -0700

    Create Classes With at Least 5 Students.sql

---
+++ Classes With at Least 5 Students.sql
@@ -0,0 +1,9 @@
+/*
+https://leetcode.com/problems/classes-with-at-least-5-students/?envType=study-plan-v2&envId=top-sql-50
+*/
+
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING COUNT(student) >=5
+
