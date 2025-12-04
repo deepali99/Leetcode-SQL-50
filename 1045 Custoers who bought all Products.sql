768e3cb0faa7004aa8c8123b4cbc4c376f5c8326
Deepali Sharma <35188717+deepali99@users.noreply.github.com>
2025-12-04 14:44:51 -0800

    Create 1045 Custoers who bought all Products.sql

---
+++ 1045 Custoers who bought all Products.sql
@@ -0,0 +1,57 @@
+/*
+Table: Customer
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| customer_id | int     |
+| product_key | int     |
++-------------+---------+
+This table may contain duplicates rows. 
+customer_id is not NULL.
+product_key is a foreign key (reference column) to Product table.
+ 
+
+Table: Product
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_key | int     |
++-------------+---------+
+product_key is the primary key (column with unique values) for this table.
+ 
+
+Write a solution to report the customer ids from the Customer table that bought all the products in the Product table.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+*/
+
+
+
+WITH cust AS 
+(
+SELECT 
+    customer_id, 
+    COUNT(DISTINCT product_key) AS num_product_key_per_cust
+FROM Customer
+GROUP BY customer_id
+),  
+
+prod AS
+(
+
+SELECT COUNT(DISTINCT product_key) AS count_distinct_product
+FROM Product
+
+)
+
+SELECT 
+    customer_id
+FROM cust JOIN prod
+WHERE num_product_key_per_cust = count_distinct_product
